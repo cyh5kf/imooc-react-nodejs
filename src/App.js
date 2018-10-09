9145155bdb22f969a354b393e27e207e085611ac
e <chenyu@51rz.com>
2018-10-09 17:28:58 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {createStore} from 'redux';
+import { connect } from 'react-redux';
+import { addGun, removeGun, addGunAsync } from './index.redux'
 
 class App extends Component {
+  // constructor(props) {
+
+  // }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>现在有机枪{this.props.num}把</h1>
+        <button onClick={() =>  this.props.addGun()}>申请武器</button>
+        <button onClick={() =>  this.props.removeGun()}>上交武器</button>
+        <button onClick={() =>  this.props.addGunAsync()}>拖两天</button>
       </div>
     );
   }
 }
 
+const mapStatetoProps = (state) => {
+  return { num: state }
+}
+
+const actionCreators = { addGun, removeGun, addGunAsync }
+
+App = connect(mapStatetoProps, actionCreators)(App)
 export default App;
