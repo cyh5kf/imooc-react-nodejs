51cfbda54d94e196b40b1a33dc8e28bec7144aa0
e <chenyu@51rz.com>
2018-10-09 18:04:01 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addGun, removeGun, addGunAsync } from './index.redux'
 
-class App extends Component {
-  // constructor(props) {
+// class App extends Component {
+//   // constructor(props) {
 
-  // }
+//   // }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>现在有机枪{this.props.num}把</h1>
+//         <button onClick={() =>  this.props.addGun()}>申请武器</button>
+//         <button onClick={() =>  this.props.removeGun()}>上交武器</button>
+//         <button onClick={() =>  this.props.addGunAsync()}>拖两天</button>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStatetoProps = (state) => {
+//   return { num: state }
+// }
+
+// const actionCreators = { addGun, removeGun, addGunAsync }
+
+// App = connect(mapStatetoProps, actionCreators)(App)
+// export default App;
+
+@connect(
+  state => ({num: state}),
+  { addGun, removeGun, addGunAsync }
+)
+class App extends Component {
 
   render() {
     return (
@@ -19,11 +46,4 @@ class App extends Component {
   }
 }
 
-const mapStatetoProps = (state) => {
-  return { num: state }
-}
-
-const actionCreators = { addGun, removeGun, addGunAsync }
-
-App = connect(mapStatetoProps, actionCreators)(App)
 export default App;
